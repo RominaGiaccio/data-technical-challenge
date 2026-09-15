b6ad84f9b9c10046cedcaa11de28880ca9c6fc93
RominaGiaccio <giacciopromina@gmail.com>
2026-09-15 16:39:40 -0300

    feat: add initial format detection

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+def detect_format(line):
+    parts = [part.strip() for part in line.split(",")]
+
+    if len(parts) == 4:
+        return "format B"
+
+    if len(parts) == 5:
+        return "format A or C"
+
+    return None
+
 def main():
     with open("input.txt", "r") as file:
         for index, line in enumerate(file):
-            print(index, line.strip())
+            clean_line = line.strip()
+            format_type = detect_format(clean_line)
+            result = f"line {index} _ {clean_line}: {format_type or 'No format detected'}"
+            print(result)
 
 
 if __name__ == "__main__":
